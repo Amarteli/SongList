4aa380778d31e93c39cc71458bd42fdfdc61554b
Amarteli <54898416+Amarteli@users.noreply.github.com>
2019-11-02 19:46:34 +0200

    Add files via upload

---
+++ src/app/config/firebase.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: 'AIzaSyDyRLEbUJFobP4wimo_UTpJ9bIKMsKrUGs',
+    authDomain: 'songlist-b8d66.firebaseapp.com',
+    databaseURL: 'https://songlist-b8d66.firebaseio.com',
+    projectId: 'songlist-b8d66',
+    storageBucket: 'songlist-b8d66.appspot.com',
+    messagingSenderId: 37129214918
+};
+
+export default firebaseConfig;
